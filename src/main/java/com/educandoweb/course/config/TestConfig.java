673213c485d462cd139e2d9c8c80c2ddbff1ad50
Myllena Medeiros <myllenamed19@gmail.com>
2025-02-03 10:45:03 -0300

    Many to many association with JoinTable

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -49,6 +49,15 @@ public class TestConfig implements CommandLineRunner {
 		
 		catRep.saveAll(Arrays.asList(cat1,cat2,cat3));
 		proRep.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		proRep.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
 		User u1 = new User(null, "Victra Barca-Julii", "bestmomofalltimes@gmail.com", "988888888","123456789");
 		User u2 = new User(null, "Sevro Barca-Julii", "howllernumber1@gmail.com", "977777777","987456321");
